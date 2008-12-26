e7f9555ddcb302da2ae5299de1126300fca7f27c
bogdan <bogdan@bogdan-desktop.(none)>
2008-12-26 23:43:58 +0200

    Added two basic tests for routing egg

--- config/ceva.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once("routes.tmp.php");
-print_r($root);
-?>
--- tests/routing_tests.php
+++ tests/routing_tests.php
@@ -2,16 +2,33 @@
 include_once(EGGS_PATH."routing.php");
 
 class TestOfRoutingEngine extends UnitTestCase{
-	function testRoutingConnect(){
+	function testRoutingDumpToFile(){
 		$routes = new Routes;
 		$routes->connect("login/:ceva",
 		                 ":controller=>login",
 				 ":action=>show");
 
-		$routes->connect("login/:id/altceva",":controller=>login",":action=>show");
-		print_r($routes->routing_tree);
-		$routes->dump_to_file("routes");
+		$routes->connect("login/:id/altceva",
+				 ":controller=>login",
+				 ":action=>show");
 
+		$routes->dump_to_file("routes");
+		include_once(SITE_PATH."config/routes.tmp.php");
+		$this->assertIdentical($routes->routing_tree,$root);
+		unlink(SITE_PATH."config/routes.tmp.php");
+
+	}
+
+	function testRoutingConnectDoubleIdentityPoint(){
+		$routes = new Routes;
+		$routes->connect("login/:ceva",
+		                 ":controller=>login",
+				 ":action=>show");
+
+		$routes->connect("login/:id/altceva",
+				 ":controller=>login",
+				 ":action=>show");
+		$this->assertTrue(count($routes->routing_tree[0][0]["name"])==2);
 	}
 }
 ?>
