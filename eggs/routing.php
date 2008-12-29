1ab0fa0063371cec3fae36d2473ff549ce8a82ec
bogdan <bogdan@bogdan-desktop.(none)>
2008-12-29 13:12:42 +0200

    Spli the routing file in routing and routes. Added real world tests for the routing process

--- eggs/routing.php
+++ eggs/routing.php
@@ -1,230 +1,82 @@
 <?php
 class Routing{
-}
+	private $url;
+	private $method;
+	private $count;
+	private $param;
 
-class NestedRoutes{
-	private $routing;
-	public $controller;
+	function __construct($request,$routes_file="routes"){
 
-	function __construct($ref){
-		$this->routing=$ref;
+		$this->load_tree($routes_file);
+
+		$this->url=explode("/",$request["url"]);
+		$this->method=$request["method"];
 	}
 
-	function __call($method,$opt){
-		switch($method){
-			case "resources":
-				$this->routing->resources($opt[0],$this->controller."/:id/");
-			break;
-			default:
-				trigger_error("No method with the name: $method",E_USER_ERROR);
-			break;
+	public function climb(){
+		$result=$this->mount($this->routing_tree,0);
+		if(!empty($this->param)){
+			$result["param"]=$this->param;
 		}
-	}
-}
-
-
-class Routes extends Routing{
-	public $routing_tree = array();
-
-	public function connect($path){
-		$opt = $this->where_to(func_get_args());
-		$this->add_to_tree($path,$opt);
+		return $result;
 	}
 
-	public function resources($controller,$header=false){
-		# index
-		$this->connect($header.$controller,
-		               ":controller=>$controller",
-			       ":action=>index",
-			       ":method=>GET");
-		# new
-		$this->connect($header.$controller."/new",
-		               ":controller=>$controller",
-			       ":action=>new",
-			       ":method=>GET");
+	private function mount($root,$part){
+		$found=-1;
+		$type=1;
 
-		# show
-		$this->connect($header.$controller."/:id",
-		               ":controller=>$controller",
-			       ":action=>show",
-			       ":method=>GET");
-		# edit
-		$this->connect($header.$controller."/:id/edit",
-		               ":controller=>$controller",
-			       ":action=>edit",
-			       ":method=>GET");
-		# create
-		$this->connect($header.$controller,
-		               ":controller=>$controller",
-			       ":action=>create",
-			       ":method=>POST");
-		# update
-		$this->connect($header.$controller."/:id",
-		               ":controller=>$controller",
-			       ":action=>update",
-			       ":method=>POST");
-		# delete
-		$this->connect($header.$controller."/:id/delete",
-		               ":controller=>$controller",
-			       ":action=>delete",
-			       ":method=>POST");
-
-		$nested = new NestedRoutes($this);
-		$nested->controller=$controller;
-		return $nested;
-	}
-
-	public function dump_to_file($file_name){
-		if(strpbrk($file_name, ".")){
-			trigger_error("Extension is not needed",E_USER_ERROR);
-		}else{
-			$handle = fopen(SITE_PATH."config/".$file_name.".tmp.php","w");
-			$this->build_output($handle);
-		}
-
-	}
-
-	private function where_to($pieces){
-		$opt = array();
-		for($i=1;$i<count($pieces);$i++){
-			$s = explode("=>",$pieces[$i]);
-			$opt[substr($s[0],1)]=$s[1];
-		}
-		return $opt;
-	}
-
-	private function add_to_tree($path,$opt){
-		$parts=explode("/",$path);
-		$nr_parts=count($parts);
-		$obj = &$this->routing_tree;
-		for($i=0;$i<$nr_parts;$i++){
-			$a = substr_count($parts[$i],":");
-			if(empty($obj)) {
-				$obj[0]=$this->create_elem($parts[$i]);
-				$obj["count"]++;
-				$current = &$obj[0];
-			}else{
-				$b=$this->check_for_part($parts[$i],$obj);
-				if($b!=-1){
-					if($b[0]==1&&empty($b[2])){
-						$obj[$b[1]]["name"][]=substr($parts[$i],1);
-					}
-					$current=&$obj[$b[1]];
-				}else{
-					$obj[]=$this->create_elem($parts[$i]);
-					$current = &$obj[$obj["count"]];
-					$obj["count"]++;
-				}
-			}
-			$obj = &$current;
-		}
-		if(!empty($opt["method"])){
-			$obj[strtolower($opt["method"])]=$opt;
-			unset($obj[strtolower($opt["method"])]["method"]);
-		}else{
-			$obj["both"]=$opt;
-		}
-	}
-
-	private function create_elem($part){
-		$a = substr_count($part,":");
-		$elem = array();
-		if($a){
-			$elem["type"]="any";
-			# FIXME needs work for more anonymous parts
-			$elem["name"]=array(substr($part,1));
-		}else{
-			$elem["type"]="normal";
-			$elem["name"]=$part;
-		}
-		$elem["count"]=0;
-		return $elem;
-	}
-
-	private function check_for_part($part,$obj){
-		$a = substr_count($part,":");
-		$found = 0;
-		$type = 0;
-		$place = 0;
-		$any_exists=FALSE;
-
-		for($i=0;$i<$obj["count"];$i++){
-			if($obj[$i]["type"]=="any"&&$a!=0){
-				$w = substr($part,1);
-				$biz = -1;
-				for($j=0;$j<count($obj[$i]["name"]);$j++){
-					if($obj[$i]["name"][$j]==$w){
-						$biz = $j;
-						break;
-					}
-				}
-				$type=1;
-				$place=$i;
-				$found=1;
-				if($biz!=-1) $any_exists=TRUE;
-				break;
-			}elseif($obj[$i]["type"]=="normal"&&$a==0){
-				if($obj[$i]["name"]==$part){
-					$found = 1;
-					$type=2;
-					$place=$i;
+		for($i=0;$i<$root["count"];$i++){
+			if($root[$i]["type"]=="normal"){
+				if($root[$i]["name"]==$this->url[$part]){
+					$found = $i;
 					break;
 				}
-			}
-		}
-		if($found==0) {
-			return -1;
-		}else{
-			$ar = array($type,$place);
-			if($any_exists!=FALSE) array_push($ar,$any_exists);
-			return $ar;
-		}
-	}
-
-	private function build_output($handle){
-		$h=$handle;
-		$root = &$this->routing_tree;
-		$output = "<?php\n";
-		$output .= '$root = array();'."\n";
-		$output .= $this->dfs($root,'$root');
-		$output .= "?>";
-		fwrite($h,$output);
-		fclose($h);
-	}
-
-	private function dfs($root,$header){
-		$output = $header .'["count"]='.$root["count"].";\n";
-		for($i=0;$i<$root["count"];$i++){
-			$x = &$root[$i];
-			$hd = $header."[".$i."]";
-			$output .= $hd ."=array();\n";
-			$output .= $hd . "[".'"type"'."]=".'"'.$x["type"].'"'.";\n";
-
-			if($x["type"]=="any"){
-				$output .= $hd . '["name"]=array();'."\n";
-				for($j=0;$j<count($x["name"]);$j++){
-					$output .= $hd . '["name"]['.$j.']='.'"'.$x["name"][$j].'"'.";\n";
-				}
 			}else{
-				$output .= $hd . "[".'"name"'."]=".'"'.$x["name"].'"'.";\n";
+				$found=$i;
+				$type=2;
+				break;
 			}
-
-			$methods=array("both","get","post");
-			$intersect = array_intersect(array_keys($x),$methods);
-			if(!empty($intersect)){
-				foreach($intersect as $v){
-					foreach($x[$v] as $key=>$value){
-						$output.=$hd . '["'.$v.'"]'.'["'.$key.'"]='.'"'.$value.'";'."\n";
-					}
-				}
-
-			}
-
-			$output .= $this->dfs($x,$hd);
 		}
-		return $output;
+
+		if($found!=-1){
+			if($root["type"]=="any"){
+				$this->param[$root["name"][$found]]=$this->url[$part-1];
+			}
+
+			if($part<count($this->url)-1){
+				return $this->mount($root[$i],$part+1);
+			}else{
+				if(!empty($root[$found][$this->method])){
+					return $root[$found][$this->method];	
+				}elseif(!empty($root[$found]["both"])){
+					return $root[$found]["both"];
+				}else{
+					return array("error"=>"No route found");
+				}
+			}
+		}else{
+			return array("error"=>"No route found");
+		}
+
+	}
+
+	private function load_tree($file){
+		if(file_exists(SITE_PATH."config/$file.tmp.php")){
+			include_once(SITE_PATH."config/$file.tmp.php");
+			$this->routing_tree=&$root;
+		}else{
+			if(empty($root)){
+				$this->load_routes($file);
+			}
+		}
+	}
+	
+	private function load_routes($file){
+		$routes = new Routes;
+		include_once(SITE_PATH."config/$file.php");
+		$routes->dump_to_file("$file");
+		$this->routing_tree=&$routes->routing_tree;
 	}
 
 }
-
 ?>
--- tests/routing_tests.php
+++ tests/routing_tests.php
@@ -1,4 +1,5 @@
 <?php
+include_once(EGGS_PATH."routes.php");
 include_once(EGGS_PATH."routing.php");
 
 
@@ -49,5 +50,28 @@ class TestOfRoutingEngine extends UnitTestCase{
 		$this->assertEqual($routes->routing_tree,$root);
 		unlink(SITE_PATH."config/nested.tmp.php");
 	}
+	function testRoutingRealWorldUsage(){
+		copy(SITE_PATH."tests/fixtures/routes.fixture.php",SITE_PATH."config/test_routes.php");
+		# test connect
+		$request=array("url"=>"login/25/dude",
+		               "method"=>"get");
+		$routing=new Routing($request,"test_routes");
+		$request_info=$routing->climb();
+		$keys = array_keys($request_info["param"]);
+
+		$this->assertEqual($keys[0],"altceva");
+		unset($routing);
+
+		# test resources
+		$request=array("url"=>"teams/25/edit",
+		               "method"=>"get");
+
+		$routing=new Routing($request,"test_routes");
+		$request_info=$routing->climb();
+		$this->assertEqual($request_info["action"],"edit");
+		unlink(SITE_PATH."config/test_routes.php");
+		unlink(SITE_PATH."config/test_routes.tmp.php");
+	}
+
 }
 ?>
